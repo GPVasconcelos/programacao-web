c213347d482cb9f361929412827b0258b9ba0f41
GPVasconcelos <gpvasconcelos@minha.fag.edu.br>
2025-03-05 20:16:25 -0300

    adicionando class e construtor

--- src/cliente/entities/cliente.entity.ts
+++ src/cliente/entities/cliente.entity.ts
@@ -1,1 +1,25 @@
-export class Cliente {}
+export class Cliente {
+    id: number;
+    nome: string;
+    cnpj: string;
+    endereco: string;
+    email: string; 
+    telefone: string;  
+
+    constructor(
+        id: number,
+        nome: string,
+        cnpj: string,
+        endereco: string,
+        email: string,
+        telefone: string,
+    ){
+        this.id = id;
+        this.nome = nome;
+        this.cnpj = cnpj;
+        this.endereco = endereco;
+        this.email = email;
+        this.telefone = telefone;
+    }
+}
+
